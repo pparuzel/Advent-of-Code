fc3f1f5d4d2ff9bebef7d7bd9700b065abfb3e2e
Pawel Paruzel <pawelparuzel95@gmail.com>
2025-12-01 14:12:47 +0000

    Replace mod tricks with standard math.fmod

--- 2025/day-01/p2.py
+++ 2025/day-01/p2.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 start = 50
 ans = 0
@@ -6,7 +7,7 @@ ans = 0
 for line in sys.stdin:
     rot = (2 * line.startswith("R") - 1) * int(line[1:])
     ans += (abs(rot) // 100)  # count in rotations by 100
-    new_start = start + (rot % 100 if rot > 0 else (rot % 100) - 100)
+    new_start = start + math.fmod(rot, 100)
     if new_start == 0 or start != 0 and (new_start < 0 or new_start > 99):
         ans += 1
     start = new_start % 100
