afcff417ed17cdecffee3144143a718780bfd414
Pawel Paruzel <pawelparuzel95@gmail.com>
2026-01-06 11:23:41 +0100

    Make it more elegant

--- 2025/day-12/p1.py
+++ 2025/day-12/p1.py
@@ -1,14 +1,13 @@
 import sys
 
-can_fit = [0, 0]  # no, yes
+ans = 0
 for line in sys.stdin:
     if "x" not in line:
         continue
     lhs, rhs = line.split(":")
     width, height = list(map(int, lhs.split("x")))
     counts = list(map(int, rhs.split()))
-    can_fit[sum(counts) * 9 <= width * height] += 1
+    ans += sum(counts) * 9 <= width * height
 
-no_fit, ans = can_fit
 print(ans)
 
